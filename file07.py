ae3ae0629fc906fa7f37fa9a1d6e3daef33e421e
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-11-14 04:54:47 +0500

    file07.py

--- file07.py
+++ file07.py
@@ -6,5 +6,17 @@ def main(data:str):
     Returns:
         int: return answer
     """
-    
+    i=0
+    sum=0
+    while i<len(data):
+        if data[i].isdigit():
+            sum+=int(data[i])
+        i+=1
+    return sum
+
+f=open('txt_file/data07.txt').read()
+data=f
+print(main(data))
+
+
 # Read data from file
