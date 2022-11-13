c5a84f955005b4a367b3d659d2b6a8e7467b55a2
Kamronbek477 <kamronbekrahmonov477@gmail.com>
2022-11-14 04:11:39 +0500

    file04.py

--- file04.py
+++ file04.py
@@ -6,5 +6,20 @@ def main(data:str):
     Returns:
         list: return answer
     """
+
+    x=[]
+    i=0
+    while i<len(data):
+        if data[i].isdigit():
+            x
+        else:
+            x.append(data[i])
+        i+=1
+    return x
+
+f=open('txt_file/data04.txt').read()
+data=f
+print(main(data))
+
     
 # Read data from file
